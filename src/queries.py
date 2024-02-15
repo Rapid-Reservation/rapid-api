cf6c7afc75b188e755dbcdcdffcd7552be857d92
Edison R Kaulfuss <104332031+ekaulfuss@users.noreply.github.com>
2024-02-15 15:08:57 -0500

    Update queries.py
    Updated queries to align with how json is put together.

--- src/queries.py
+++ src/queries.py
@@ -1,5 +1,5 @@
 INSERT_RESERVATION = "INSERT INTO public.table (table_id) VALUES (%s);"
 
-SELECT_RESERVATION = "SELECT * FROM public.table WHERE table_id = %s;"
+SELECT_RESERVATION = "SELECT table_id, table_available FROM public.table WHERE table_id = %s;"
 
-GET_TABLE_INFO = "SELECT * FROM public.table;"
+GET_TABLE_INFO = "SELECT table_id, table_available FROM public.table;"
