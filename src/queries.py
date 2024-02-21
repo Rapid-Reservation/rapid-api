28f98fac763c8754ae0e93c85ba0c9398460c818
Anthony E. <AnthonyDEpps@gmail.com>
2024-02-21 01:12:27 -0500

    chore: major refactor to accomodate live update via UI

--- src/queries.py
+++ src/queries.py
@@ -1,5 +1,7 @@
-INSERT_RESERVATION = "UPDATE public.table SET table_available = False WHERE table_id = %s;"
+SET_RESERVATION = "UPDATE public.table SET table_available = False WHERE table_id = %s;"
+
+CLEAR_RESERVATION = "UPDATE public.table SET table_available = True WHERE table_id = %s;"
 
 SELECT_RESERVATION = "SELECT * FROM public.table WHERE table_id = %s;"
 
-GET_TABLE_INFO = "SELECT * FROM public.table;"
+GET_TABLE_INFO = "SELECT * FROM public.table ORDER BY table_id;"
