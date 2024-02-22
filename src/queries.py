40e54233f1faf9f0a18d74ac96eb1d475296156c
Edison R Kaulfuss <eddie.kaulfuss@gmail.com>
2024-02-22 04:28:24 -0500

    Added Google Style docstrings

--- src/queries.py
+++ src/queries.py
@@ -1,3 +1,7 @@
+"""
+Collection of CONSTANTS that define sql queries.
+"""
+
 SET_RESERVATION = "UPDATE public.table SET table_available = False WHERE table_id = %s;"
 
 CLEAR_RESERVATION = "UPDATE public.table SET table_available = True WHERE table_id = %s;"
