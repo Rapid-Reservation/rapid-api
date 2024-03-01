180cad81b81cf91e23cad42e2d741c05d42be7c7
Anthony E. <AnthonyDEpps@gmail.com>
2024-02-29 22:27:10 -0500

    feat: add basic order endpoints

--- src/queries.py
+++ src/queries.py
@@ -1,7 +1,9 @@
 """
 Collection of CONSTANTS that define sql queries.
 """
-
+"""
+Reservation Queries
+"""
 SET_RESERVATION = "UPDATE public.table SET table_available = False WHERE table_id = %s;"
 
 CLEAR_RESERVATION = "UPDATE public.table SET table_available = True WHERE table_id = %s;"
@@ -9,3 +11,11 @@ CLEAR_RESERVATION = "UPDATE public.table SET table_available = True WHERE table_
 SELECT_RESERVATION = "SELECT * FROM public.table WHERE table_id = %s;"
 
 GET_TABLE_INFO = "SELECT * FROM public.table ORDER BY table_id;"
+
+"""
+Order queries
+"""
+
+SELECT_ORDER = "SELECT * FROM public.order WHERE order_id = %s;"
+
+GET_ORDER_INFO = "SELECT * FROM public.order ORDER BY order_id;"
