5c2621718a5bee1272c60f8d4035408c5fdb077e
Edison R Kaulfuss <eddie.kaulfuss@gmail.com>
2024-03-21 19:29:59 -0400

    Feat: Added customer endpoints

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ from flask_caching import Cache
 from psycopg2 import pool
 
 # Local Modules
-from queries import SET_RESERVATION, SELECT_RESERVATION, GET_TABLE_INFO, CLEAR_RESERVATION
+from queries import SET_RESERVATION, SELECT_RESERVATION, GET_TABLE_INFO, CLEAR_RESERVATION, GET_ALL_CUSTOMERS, GET_CUSTOMER_BY_ID, CREATE_NEW_CUSTOMER
 import pool
 import logger
 
@@ -133,6 +133,88 @@ def get_table_info():
         return jsonify({'error': 'Internal Server Error'}), 500
     finally:
         pool.release_connection(connection)
+
+@app.route('/customer', methods=['GET'])
+def get_customer_info():
+    """
+    This route is used to pull all the data from all the customers. Calls GET_ALL_CUSTOMERS from queries.py
+
+    Returns:
+        Json of all customer data on success
+        Fail message on Failure
+    """
+    customers = {}
+    try:
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(GET_ALL_CUSTOMERS)
+        results = cursor.fetchall()
+        
+        customers = [{'customer_id': row[0],'customer_name': row[1], 'customer_address': row[2], 'customer_phone': row[3], 'customer_email': row[4]} for row in results]
+        return jsonify(customers)
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+    finally:
+        pool.release_connection(connection)
+    
+
+@app.route('/customer/<int:customer_id>', methods=['GET'])
+def get_one_customer_info(customer_id):
+    """
+    This route is used to return a single customers information. Calls GET_CUSTOMER_BY_ID from queries.py
+
+    Returns:
+        Json with customer information on success
+        Error message on error
+    """
+    try:
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(GET_CUSTOMER_BY_ID, (customer_id,))
+        result = cursor.fetchall()
+
+        customer = [{'customer_id': row[0],'customer_name': row[1], 'customer_address': row[2], 'customer_phone': row[3], 'customer_email': row[4]} for row in result]
+
+        return jsonify(customer)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+    finally:
+        pool.release_connection(connection)
+
+
+@app.route('/customer/set', methods=['POST'])
+# @cross_origin()
+def new_customer():
+    """
+    This route can be called to add a new customer. Calls CREATE_NEW_CUSTOMER in queries.py
+
+    Returns:
+        Success message on success
+        Error message on Error
+    """
+    try:
+        data = request.json
+        customer_id = data.get('customer_id')
+        customer_name = data.get('customer_name')
+        customer_address = data.get('customer_address')
+        customer_phone = data.get('customer_phone')
+        customer_email = data.get('customer_email')
+        print(customer_name + customer_address + customer_phone + customer_email)
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(CREATE_NEW_CUSTOMER, (customer_id, customer_name, customer_address, customer_phone, customer_email,))
+        connection.commit()
+        return jsonify({'success': True, 'message': 'Customer added successfully'})
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+    # finally:
+        # pool.release_connection(connection)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
