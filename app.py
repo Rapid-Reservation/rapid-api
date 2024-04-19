e279edfc699bf527d6ce6d4079b1a35e41c14578
Anthony E. <AnthonyDEpps@gmail.com>
2024-04-19 19:18:40 -0400

    fix: attempt to fix auth

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # Imported Modules
+import os
+from typing import Annotated
+from dotenv import load_dotenv
 from models import Order
-from typing import Union
-from urllib import request
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, HTTPException, Depends, status
 from fastapi.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -11,13 +11,14 @@ from psycopg2 import pool
 from datetime import datetime, timedelta
 from jose import JWTError, jwt  
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import uvicorn
 # Local Modules
 import queries as q
 import pool
-import models
 # from logging.handlers import RotatingFileHandler - stupid Vercel
 
+load_dotenv()
 app = FastAPI()
 
 # origins = [
@@ -44,6 +45,131 @@ This route is the base route for API
 @app.get("/")
 def root():
     return {"status":"Rapid Reservation API is running"}
+
+
+"""
+Authentication
+"""
+class User(BaseModel):
+    id: int
+    email: str
+    password: str
+    isadmin: bool
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+SECRET_KEY=os.getenv('SECERT_KEY')
+ALGORITHM=os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def hash_password(password: str):
+    """
+    Function takes a passed password and hashes it \n
+    Returns:
+        JSON of all table data on success
+        Fail message on Failure
+    """
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    """
+    Function takes a passed password and password hashed and confirms they are the same
+    Returns:
+        JSON of all table data on success
+        Fail message on Failure
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.now(datetime.UTC)+ expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# Function to authenticate a user
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user or not verify_password(password, user.get("password_hash")):
+        return False
+    return user
+
+
+# Function to get a user from the database
+def get_user(username: str):
+    try:
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(q.GET_USER_BY_USERNAME, (username))
+        user_data = cursor.fetchone()
+
+        if user_data:
+            # Extract user data into a dictionary
+            user = {
+                "user_name": user_data[1],
+                "password": user_data[2],
+            }
+            return user
+        else:
+            return None  # Return None if user not found
+        
+    finally:
+        pool.release_connection(connection)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+# Endpoint for user login
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+# Endpoint for token refresh (if needed)
+@app.post("/token/refresh")
+async def refresh_access_token(current_user: User = Depends(get_current_user)):
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": current_user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+
+"""
+TABLES
+"""
 
 """
 This route is called when a table is reserved. Calls SET_RESERVATION script in queries.py
@@ -142,6 +268,57 @@ def get_table_info():
     finally:
         pool.release_connection(connection)
 
+"""
+USERS
+"""
+@app.get('/users')
+def get_user():
+    """
+
+    """
+    users={}
+    try:
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(q.GET_ALL_USERS)
+        results = cursor.fetchall()
+        
+        return [{"user_id": row[0], "user_name": row[1], "password": row[2], "isadmin": row[3]} for row in results]
+        return users
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return {'error': 'Internal Server Error'}, 500
+    finally:
+        pool.release_connection(connection)
+
+@app.post('/users/create')
+async def new_customer(request: Request):
+    """
+    This route can be called to add a new customer. Calls CREATE_NEW_CUSTOMER in queries.py
+    Returns:
+        Success message on success
+        Error message on Error
+    """
+    try:
+        data = await request.json()
+        user_id = data.get('user_id')
+        user_name = data.get('user_name')
+        password = data.get('password')
+        isadmin = data.get('isadmin')
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(q.CREATE_NEW_USER, ((user_id, user_name, password,isadmin)))
+        connection.commit()
+        return {'success': True, 'message': 'User {user_id} added successfully'}
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail='Internal Server Error')
+
+"""
+CUSTOMERS
+"""
+
 @app.get('/customer')
 def get_customer_info():
     """
@@ -217,6 +394,9 @@ async def new_customer(request: Request):
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail='Internal Server Error')
     
+"""
+ORDERS
+"""
 
 @app.get("/orders")
 def get_orders():
--- queries.py
+++ queries.py
@@ -2,6 +2,18 @@
 Collection of CONSTANTS that define sql queries.
 """
 
+"""
+User based queries
+"""
+
+GET_ALL_USERS = "SELECT * from public.user"
+GET_USER_BY_ID = "SELECT * from public.user WHERE user_id = %s"
+GET_USER_BY_USERNAME = "SELECT * from public.user WHERE user_name = %s"
+CREATE_NEW_USER= "INSERT INTO public.user (user_id, user_name, password, isadmin) VALUES (%s, %s, %s, %s);"
+
+"""
+Table based queries
+"""
 SET_RESERVATION = "UPDATE public.table SET table_available = False WHERE table_id = %s;"
 
 CLEAR_RESERVATION = "UPDATE public.table SET table_available = True WHERE table_id = %s;"
@@ -10,12 +22,15 @@ SELECT_RESERVATION = "SELECT * FROM public.table WHERE table_id = %s;"
 
 GET_TABLE_INFO = "SELECT * FROM public.table ORDER BY table_id;"
 
+""" Customer Based Queries"""
 GET_ALL_CUSTOMERS = "SELECT * FROM public.customer ORDER BY customer_id;"
 
 GET_CUSTOMER_BY_ID = "SELECT * FROM public.customer WHERE customer_id = %s;"
 
 CREATE_NEW_CUSTOMER = "INSERT INTO public.customer (customer_id, customer_name, customer_address, customer_phone, customer_email) VALUES (%s, %s, %s, %s, %s);"
 
+
+""" Order based Queries"""
 GET_ALL_ORDERS = "SELECT * FROM orders;"
 
 GET_ORDER_BY_ID = "SELECT * FROM orders WHERE order_id = %s;"
